3164af84d37f3ea2528f111ea6102e5665da43ec
v0lFr4m <iphoneivan14@gmail.com>
2025-06-08 17:10:02 +0300

    Guess a number python mini game

---
+++ guess_a_number/guess_a_number.py
@@ -0,0 +1,46 @@
+import random
+computer_number = random.randint(1, 100)
+
+print('|==============================================================================|')
+print('|--------------------- Welcome to game Guess the number -----------------------|')
+print('|==============================================================================|')
+print('|---------- The rules are simple, you need to enter number --------------------|')
+print('|--------------- and the computer will give you a hint ------------------------|')
+print('|==============================================================================|')
+
+current_points = 0
+while True:
+    player_input = input('Guess the number (1 - 100): ')
+    if not player_input.isdigit():
+        print('|==============================================================================|')
+        print('|--------------- You need to enter valid number, Try again... -----------------|')
+        print('|==============================================================================|')
+        continue
+    player_number = int(player_input)
+
+    if player_number < 0 or player_number > 100:
+        print('|----------- You must enter number in range 1 - 100, Try again... -------------|')
+        continue
+
+    if player_number == computer_number:
+        print()
+        print('|==============================================================================|')
+        print('|------------------------- You Guessed the number!-----------------------------|')
+        print('|----------------------- Do you want to play again ? --------------------------|')
+        print('|-------------------------------- YES / NO ------------------------------------|')
+        print('|==============================================================================|')
+        print()
+        current_points += 10
+        player_input = input('You choose : ').lower()
+        print()
+        if player_input == 'yes':
+            continue
+        else:
+            print ('|==============================================================================|')
+            print(f'|-------------------- TOTAL POINTS : {current_points} ---------------------------------------|')
+            print ('|==============================================================================|')
+            break
+    elif player_number < computer_number:
+        print('|------------------------- The number is too low ! ----------------------------|')
+    else:
+        print('|------------------------- The number is too high ! ---------------------------|')
